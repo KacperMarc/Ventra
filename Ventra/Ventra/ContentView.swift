dcbcab6d79ff187c1359c1707d2cfba582f82ebb
KacperMarc <nvidia.getaway@gmail.com>
2025-07-08 20:58:03 +0200

    Project setup

---
+++ Ventra/Ventra/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ventra
+//
+//  Created by Kacper Marciszewski on 08/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ventra/Ventra/VentraApp.swift
@@ -0,0 +1,17 @@
+//
+//  VentraApp.swift
+//  Ventra
+//
+//  Created by Kacper Marciszewski on 08/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct VentraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
