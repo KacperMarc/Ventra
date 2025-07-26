65e5f7ab625ce6157553add21ceeba45810a77a4
KacperMarc <nvidia.getaway@gmail.com>
2025-07-27 00:57:34 +0200

    Setup Turbine Information View

--- Ventra/Ventra/Features/TurbineInfo/View/TurbineInfoView.swift
+++ Ventra/Ventra/Features/TurbineInfo/View/TurbineInfoView.swift
@@ -7,6 +7,59 @@
 
 import SwiftUI
 
+struct TurbineInfoCard: View {
+    
+    var turbine: Turbine
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 8) {
+            
+                
+            VStack {
+                    Image(systemName: "bolt.square")
+                        .foregroundStyle(.blue)
+                    Text("Current output")
+                    .fontWeight(.semibold)
+                Text(String(turbine.power)).font(.caption)
+                    
+                }
+            .padding([.top, .bottom])
+
+                .background(Color.secondary.opacity(0.05))
+                .cornerRadius(16)
+            
+            
+            
+                
+            VStack {
+                Image(systemName: "checkmark.rectangle.stack")
+                    .foregroundStyle(.green)
+                Text("Efficency").fontWeight(.semibold)
+                Text(String(turbine.efficiency)).font(.caption)
+            }
+            .padding([.top, .bottom])
+            .background(Color.secondary.opacity(0.05))
+            .cornerRadius(16)
+            
+                
+                
+            VStack {
+                Image(systemName: "wind")
+                    .foregroundStyle(.cyan)
+                Text("Speed").fontWeight(.semibold)
+                Text(String(turbine.speed)).font(.caption)
+                    
+            }
+            .padding([.top, .bottom], 4)
+            .background(Color.secondary.opacity(0.05))
+            .cornerRadius(16)
+            
+            
+        }
+        .padding([.leading, .trailing, .bottom])
+    }
+}
+
 struct TurbineInfoView: View {
     
     var turbine: Turbine
@@ -17,11 +70,39 @@ struct TurbineInfoView: View {
     
     var body: some View {
         VStack {
-            VStack {
-                Text("\(turbine.turbineId)")
+           
+                VStack(alignment: .leading) {
+                    VStack{
+                        HStack{
+                            Text("\(turbine.name)")
+                                .font(.system(size: 28, weight: .bold))
+                                
+                            Spacer()
+                            HStack{
+                                Image(systemName: turbine.status.systemImage)
+                                    .foregroundColor(turbine.status.color)
+                                Text(turbine.status.displayName)
+                                    .foregroundColor(turbine.status.color).font(.headline)
+                            }.padding(.all, 4)
+                                .background(turbine.status.color.opacity(0.15)).clipShape(Capsule())
+                        }
+                        HStack {
+                            Text("ID: \(turbine.turbineId)")
+                                .font(.caption)
+                            Spacer()
+                            Text("Last update: 1s ago")
+                                .font(.caption)
+                                
+                        }
+                        TurbineInfoCard(turbine: turbine)
+                        
+                        
+                    }.padding()
+                }.background(Color.white)
+                    .cornerRadius(16)
                 //główne informacje ze statusem i danymi
                 //scroll view horyzontalny z info o wydajnosci, utrzymanie i serwis oraz lokalizacji
-            }
+            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .ignoresSafeArea(.all)
@@ -31,5 +112,5 @@ struct TurbineInfoView: View {
 }
 
 #Preview {
-    TurbineInfoView(turbine: sampleTurbines[0])
+    TurbineInfoView(turbine: sampleTurbines[2])
 }
